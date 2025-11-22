145827710e08ac333041b0a12767e207bc23f993
Davi Santos <160155826+dvsantoss@users.noreply.github.com>
2025-11-22 15:16:49 -0300

    update questoes_screen

---
+++ teste_tela_enem/src/main/java/br/ufrn/tads/controllers/QuestoesController.java
@@ -0,0 +1,85 @@
+package br.ufrn.tads.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.text.Text;
+
+public class QuestoesController {
+
+    @FXML
+    private Button btn_ajuda;
+
+    @FXML
+    private Button btn_calendario;
+
+    @FXML
+    private Button btn_cht;
+
+    @FXML
+    private Button btn_cnt;
+
+    @FXML
+    private Button btn_lct;
+
+    @FXML
+    private Button btn_menu;
+
+    @FXML
+    private Button btn_mt;
+
+    @FXML
+    private Button btn_provas;
+
+    @FXML
+    private Button btn_questoes;
+
+    @FXML
+    private Text nomeUser;
+
+    @FXML
+    void ajuda_screen(ActionEvent event) {
+
+    }
+
+    @FXML
+    void calendario__screen(ActionEvent event) {
+
+    }
+
+    @FXML
+    void cht_questions(ActionEvent event) {
+
+    }
+
+    @FXML
+    void cnt_questions(ActionEvent event) {
+
+    }
+
+    @FXML
+    void lct_questions(ActionEvent event) {
+
+    }
+
+    @FXML
+    void menu_screen(ActionEvent event) {
+
+    }
+
+    @FXML
+    void mt_questions(ActionEvent event) {
+
+    }
+
+    @FXML
+    void prova_screen(ActionEvent event) {
+
+    }
+
+    @FXML
+    void questoes_screen(ActionEvent event) {
+
+    }
+
+}
